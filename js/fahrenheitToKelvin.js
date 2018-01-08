c5f7dac0b53db26d02e45c237aec2798fb71b5b9
Elizabeth <ferrao.elizabeth@gmail.com>
2018-01-08 09:47:40 -0500

    deleted internal js folder

--- js/fahrenheitToKelvin.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let fahrenheit = prompt("what is the temperature today in Fahrenheit?");
-
-let celsius = ((fahrenheit * (9/5)) + 32);
-
-let kelvin = celsius + 273;
-
-console.log(`${fahrenheit}° Fahrenheit = ${celsius}° Celsius = ${kelvin}° Kelvin`)
